40dac55dfc134e4b69824a00e5297657ad760580
Rob LaFeve <roblafeve@gmail.com>
2023-01-27 16:50:47 -0600

    Fix playwright command

--- playwright.config.ts
+++ playwright.config.ts
@@ -99,7 +99,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "npm start",
     port: 3000,
     reuseExistingServer: true,
   },
